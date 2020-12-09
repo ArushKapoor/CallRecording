d5873395693577154e549612de800e2eaa2685dd
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-12-10 02:36:23 +0530

    setup basic structure to record and play audio.

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
 
 class HomePage extends StatefulWidget {
   static const String id = 'home_page';
@@ -7,10 +8,131 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  FlutterSoundPlayer _myPlayer = FlutterSoundPlayer();
+  FlutterSoundRecorder _myRecorder = FlutterSoundRecorder();
+  bool _mPlayerIsInited;
+  bool _mRecorderIsInited;
+
+  @override
+  void initState() {
+    super.initState();
+    _myPlayer.openAudioSession().then((value) {
+      setState(() {
+        _mPlayerIsInited = true;
+      });
+    });
+    _myRecorder.openAudioSession().then((value) {
+      setState(() {
+        _mRecorderIsInited = true;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _myPlayer.closeAudioSession();
+    _myPlayer = null;
+    _myRecorder.closeAudioSession();
+    _myRecorder = null;
+    super.dispose();
+  }
+
+  void play() async {
+    await _myPlayer.startPlayer(
+        // fromURI: _exampleAudioFilePathMP3,
+        codec: Codec.mp3,
+        whenFinished: () {
+          setState(() {});
+        });
+    setState(() {});
+  }
+
+  Future<void> stopPlayer() async {
+    if (_myPlayer != null) {
+      await _myPlayer.stopPlayer();
+    }
+  }
+
+  Future<void> record() async {
+    await _myRecorder.startRecorder(
+      // toFile: _mPath,
+      codec: Codec.aacADTS,
+    );
+  }
+
+  Future<void> stopRecorder() async {
+    await _myRecorder.stopRecorder();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal,
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Call Recorder'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              play();
+            },
+            child: Container(
+              margin: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Text('Play Sound'),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              stopPlayer();
+            },
+            child: Container(
+              margin: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Text('Stop Sound'),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              record();
+            },
+            child: Container(
+              margin: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Text('Record Audio'),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              stopRecorder();
+            },
+            child: Container(
+              margin: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Text('Stop Recording Audio'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
